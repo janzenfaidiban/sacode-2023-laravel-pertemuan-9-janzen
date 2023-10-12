6cc0406ab1032e0e9b04b6c8c4cff4bbe28d42f9
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-10-12 20:20:33 +0900

    updated routes

---
+++ resources/views/admin/pages/developers/index.blade.php
@@ -0,0 +1,3 @@
+<h1>
+    Developer > Index
+</h1>
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\DevelopersController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// admin/developers
+Route::get('admin/developers', [DevelopersController::class, 'index']);
